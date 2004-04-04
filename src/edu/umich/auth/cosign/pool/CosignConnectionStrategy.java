f6a740bd49389b124798cc84a831bbe774c9826a
htchan <htchan>
2004-04-04 06:28:19 +0000

    *** empty log message ***

--- src/edu/umich/auth/cosign/pool/CosignConnectionStrategy.java
+++ src/edu/umich/auth/cosign/pool/CosignConnectionStrategy.java
@@ -1,15 +1,61 @@
 package edu.umich.auth.cosign.pool;
 
-import java.util.TreeMap;
+import java.util.LinkedList;
+import java.util.List;
 
-/**
- * @author htchan *  * To change this generated comment edit the template variable "typecomment": * Window>Preferences>Java>Templates. * To enable and disable the creation of type comments go to * Window>Preferences>Java>Code Generation.
- * 
- * @uml.stereotype name="tagged" isDefined="true" 
- * @uml.stereotype name="interface" 
+/*
+ * Created on Apr 1, 2004
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
  */
+/**
+ * @author htchan
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class CosignConnectionStrategy {
+	
+	private List poolKeyList;
+	private Object firstPoolKey;
+	private boolean firstTime;
 
-public interface CosignConnectionStrategy {
-	public CosignConnection getConnection(TreeMap poolMap);
-	public void reset();
+	public CosignConnectionStrategy() {
+		super();
+		this.poolKeyList = new LinkedList(CosignConnectionPoolManager.INSTANCE.getPoolKeySet());
+		this.firstPoolKey = poolKeyList.get(0);
+		this.firstTime = true;
+	}
+
+	public synchronized CosignConnection getConnection() {
+		CosignConnectionPool pool = getConnectionPool();
+		CosignConnection cc = null;
+		
+		if (null != pool) {
+			try {
+				cc = (CosignConnection) pool.borrowObject();
+				cc.setThePool(pool);
+			}
+			catch (Exception e) {
+				System.out.println("Fail to borrow connection from " + pool.getCosignConnectionPoolName() + "!");
+				return getConnection();
+			}
+		}
+		return cc;
+	}
+	
+	private CosignConnectionPool getConnectionPool() {
+
+		Object poolKey = poolKeyList.get(0);
+		CosignConnectionPool pool = null;
+		if (firstTime || firstPoolKey != poolKey) {
+			firstTime = false;
+			pool = CosignConnectionPoolManager.INSTANCE.getConnectionPool(poolKey);
+			poolKeyList.remove(0);
+			poolKeyList.add(poolKey);
+		}
+		return pool;
+	}
+
 }
