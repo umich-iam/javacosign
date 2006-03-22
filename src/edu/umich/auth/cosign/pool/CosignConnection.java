03520d5516d463bd3e860019a84b614cb2ba00ca
patkm <patkm>
2006-03-22 20:23:16 +0000

    Code update to 1.5 beta

--- src/edu/umich/auth/cosign/pool/CosignConnection.java
+++ src/edu/umich/auth/cosign/pool/CosignConnection.java
@@ -11,15 +11,16 @@ import java.io.*;
 /**
  * This class represents a Cosign Connection.  It creates a socket
  * connection to the cosign server, converts to an SSL connection
- * and then starts a TLS handshake.  It uses to check whether a 
+ * and then starts a TLS handshake.  It uses to check whether a
  * cosign service cookie is valid or not.  Once the check is over,
  * it will return the connection to the connection pool.
- * 
- * @author dillaman * 
- * @uml.stereotype name="tagged" isDefined="true" 
+ *
+ * @author dillaman
+ *
+ * @uml.stereotype name="tagged" isDefined="true"
  */
 public class CosignConnection {
-  
+
   // Codes received from the Cosign server.
   public static final int COSIGN_CODE_UNKNOWN           = -1;
   public static final int COSIGN_USER_AUTHENTICATED     = 2;
@@ -33,22 +34,22 @@ public class CosignConnection {
    * The unique pool Id and ip addr
    */
   private final String cosignConId;
-  
+
   /**
    * The host addr of the cosign server
    */
   private final String hostAddr;
-  
+
   /**
    * The port number of the cosign server, default to 6663
    */
   private final int port;
-  
+
   /**
    * Used to receive response from the cosign server in Non-SSL mode
    */
   private BufferedReader in;
-  
+
   /**
    * Used to send query to the cosign server in Non-SSL mode
    */
@@ -65,7 +66,7 @@ public class CosignConnection {
    * Used to receive response from the cosign server in SSL mode
    */
   private BufferedReader sin;
-  
+
   /**
    * Used to send query to the cosign server in SSL mode
    */
@@ -78,16 +79,17 @@ public class CosignConnection {
 
   /**
    * Constructor for CosignConnection.
-   * @param hostname   Cosign Server Hostname 
+   * @param hostname   Cosign Server Hostname
    * @param port    Cosign Server port
    */
   public CosignConnection( String cosignConListId, String hostAddr, int port ) throws IOException {
     this.cosignConId = cosignConListId + ":" + hostAddr + ":"  + port;
     this.hostAddr = hostAddr;
     this.port = port;
+
     init();
   }
-  
+
   /**
    * This methods returns the cosign connection ID
    * @return String The cosign connection ID
@@ -95,7 +97,7 @@ public class CosignConnection {
   public String getCosignConId() {
     return cosignConId;
   }
-  
+
   /**
    * This method returns the host IP address
    * @return String The host IP address
@@ -103,11 +105,11 @@ public class CosignConnection {
   public String getHostAddress () {
     return hostAddr;
   }
-  
+
   /**
    * This method converts a Cosign response string into an int code.
    * @param cosignResponse String The response received from checkCookie
-   * @return int The code for the given cosignResponse.  Returns 
+   * @return int The code for the given cosignResponse.  Returns
    *    COSIGN_CODE_DEFAULT if cosignResponse is null
    */
   public static int convertResponseToCode( String cosignResponse ) {
@@ -138,19 +140,19 @@ public class CosignConnection {
       if (log.isDebugEnabled()) {
         log.debug("[" + cosignConId + "] CHECK " + serviceName + "=" + cookie);
       }
-      
-      // Send the cookie to the cosign server 
+
+      // Send the cookie to the cosign server
       this.sout.println("CHECK " + serviceName + "=" + cookie);
       this.sout.flush();
-      
+
       // Gets the result from the cosign server
       String cosignResponse = sin.readLine();
-      
-      if (log.isDebugEnabled()) { 
+
+      if (log.isDebugEnabled()) {
         log.debug("[" + cosignConId + "] result CHECK: " + cosignResponse);
-      }    
+      }
       return cosignResponse;
-      
+
     } catch (Exception e) {
       if ( log.isDebugEnabled() ) {
         log.debug("[" + cosignConId + "] failed while validating cookie with cosign server", e);
@@ -158,9 +160,9 @@ public class CosignConnection {
       return null;
     }
   }
-  
+
   /**
-   * This methods check whether the secure Cosign connection is 
+   * This methods check whether the secure Cosign connection is
    * still alive by issuing a NOOP request to the Cosign server.
    * @return  True if the socket/server is alive. False otherwise.
    */
@@ -169,7 +171,7 @@ public class CosignConnection {
       if (log.isDebugEnabled()) {
         log.debug("[" + cosignConId + "] NOOP");
       }
-      
+
       this.sout.println("NOOP");
       this.sout.flush();
       String result = sin.readLine();
@@ -182,7 +184,7 @@ public class CosignConnection {
       }
       return true;
     }
-    
+
     catch ( Exception e ) {
       if ( log.isDebugEnabled() ) {
         log.debug("[" + cosignConId + "] failed while checking connection to cosign server", e);
@@ -190,7 +192,7 @@ public class CosignConnection {
       return false;
     }
   }
-  
+
   /**
    * This method hard closes the Cosign connections.
    */
@@ -198,7 +200,7 @@ public class CosignConnection {
     if ( log.isDebugEnabled() ) {
       log.debug( "[" + cosignConId + "]: hard closing cosign connection" );
     }
-    
+
     try {
       if (null != this.sout) {
         this.sout.close();
@@ -238,11 +240,11 @@ public class CosignConnection {
     try{
       // Creates non-SSL socket
       Socket s = new Socket(hostAddr, port);
-      
+
       // Gets the input/output reader/writer
       this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
       this.out = new PrintWriter(s.getOutputStream());
-      
+
       // This reads welcome message from the cosign server
       String response = in.readLine();
       if (log.isDebugEnabled()) {
@@ -250,31 +252,31 @@ public class CosignConnection {
         log.debug("[" + cosignConId + "]: reading banner!");
         log.debug("[" + cosignConId + "]: result: " + response);
       }
-      
+
       // Notifies the cosign to begin SSL communication
       out.println("STARTTLS");
       out.flush();
-      
+
       // Get cosign server's response
       response = in.readLine();
       if (log.isDebugEnabled()) {
         log.debug("[" + cosignConId + "]: result STARTTLS: " + response);
       }
-      
+
       // Convert existing socket to an SSL one
       SSLSocket ss = CosignSSLSocketFactory.INSTANCE.createSSLSocket(s, hostAddr, port, true);
 
       // Shows all the cipher suites
-      if (log.isDebugEnabled()) {
+      if (true){ //(log.isDebugEnabled()) {
         String[] cipherSuites = ss.getEnabledCipherSuites();
         for (int i = 0; i < cipherSuites.length; i++) {
-          log.debug("[" + cosignConId + "]: enabled Ciper Suite: " + cipherSuites[i]);
+          log.info("[" + cosignConId + "]: enabled Ciper Suite: " + cipherSuites[i]);
         }
       }
-      
+
       // Start the SSL handshake
       ss.startHandshake();
-      
+
       // Set the SSL input/output reader/writer
       sin = new BufferedReader(new InputStreamReader(ss.getInputStream()));
       sout = new PrintWriter(ss.getOutputStream());
