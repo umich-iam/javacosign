db6cdb22b11b3a4831125f01011fac5e04792848
patkm <patkm>
2009-03-27 19:18:20 +0000

    Version 3.0

--- src/edu/umich/auth/ServletCallbackHandler.java
+++ src/edu/umich/auth/ServletCallbackHandler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import java.util.Map;
 
-
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
@@ -14,7 +13,6 @@ import javax.servlet.ServletException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import edu.umich.auth.cosign.CosignPrincipal;
 
 /**
  * @see javax.security.auth.callback.CallbackHandler
@@ -36,3 +34,22 @@ public interface ServletCallbackHandler extends CallbackHandler
   HttpServletResponse getResponse();
   void handle( Callback[] callbacks ) throws IOException, UnsupportedCallbackException;
 }
+
+/*Copyright (c) 2002-2008 Regents of The University of Michigan.
+All Rights Reserved.
+
+    Permission to use, copy, modify, and distribute this software and
+    its documentation for any purpose and without fee is hereby granted,
+    provided that the above copyright notice appears in all copies and
+    that both that copyright notice and this permission notice appear
+    in supporting documentation, and that the name of The University
+    of Michigan not be used in advertising or publicity pertaining to
+    distribution of the software without specific, written prior
+    permission. This software is supplied as is without expressed or
+    implied warranties of any kind.
+
+The University of Michigan
+c/o UM Webmaster Team
+Arbor Lakes
+Ann Arbor, MI  48105
+*/
